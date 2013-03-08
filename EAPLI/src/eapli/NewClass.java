2c1429741902d28afb44450b9f98bc9754769417
Joás V. Pereira <joasdavid@gmail.com>
2013-03-08 19:41:39 +0000

    issue #1 corrigido

--- EAPLI/src/eapli/NewClass.java
+++ EAPLI/src/eapli/NewClass.java
@@ -24,7 +24,7 @@ public class NewClass {
     
     public static void main(String[] args) {
         for (int i = 0; i  < 10; i++){
-            System.out.println("--->"+i);
+            System.out.println("--->"+(1+i));
         }
     }
 
